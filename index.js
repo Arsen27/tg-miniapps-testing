96b0831847f4a72aa1e19490113f27923ed3ff5c
Arsen <arsen.palamarchuk@gmail.om>
2023-09-23 14:13:19 +0200

    0

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 const tg = window.Telegram.webApp
 
+document.getElementById('p').innerText = `${tg.initDataUnsafe.user.first_name}`
+
 console.log('initData', tg.initData)
 console.log('initDataUnsafe', tg.initDataUnsafe)
+
