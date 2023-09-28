f6dd3a7b85690f15f03b60ec5d6733143498b3c3
Arsen <arsen.palamarchuk@gmail.om>
2023-09-28 18:21:28 +0200

    map remove

--- index.js
+++ index.js
@@ -1,24 +1,24 @@
 console.log('hello')
 document.getElementById('p').innerText = `іпаііва`
 
-import Map from './node_modules/ol/Map.js';
-import View from './node_modules/ol/View.js';
-import TileLayer from './node_modules/ol/layer/Tile.js';
-import OSM from './node_modules/ol/source/OSM.js';
+// import Map from './node_modules/ol/Map.js';
+// import View from './node_modules/ol/View.js';
+// import TileLayer from './node_modules/ol/layer/Tile.js';
+// import OSM from './node_modules/ol/source/OSM.js';
 // import XYZ from './libs/ol/source/XYZ.js';
 
-const map = new Map({
-  target: 'map',
-  layers: [
-    new TileLayer({
-      source: new OSM(),
-    }),
-  ],
-  view: new View({
-    center: [0, 0],
-    zoom: 2,
-  }),
-});
+// const map = new Map({
+//   target: 'map',
+//   layers: [
+//     new TileLayer({
+//       source: new OSM(),
+//     }),
+//   ],
+//   view: new View({
+//     center: [0, 0],
+//     zoom: 2,
+//   }),
+// });
 
 const tg = window.Telegram.WebApp
 
